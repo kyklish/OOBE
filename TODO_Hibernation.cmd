930abd414c78109266c118c2d08604f3e8e29824
kyklish <kyklish@gmail.com>
2025-02-19 18:36:25 +0200

    TODO

--- TODO_Hibernation.cmd
+++ TODO_Hibernation.cmd
@@ -3,7 +3,7 @@
 @REM     Standby (S3)
 @REM     Hibernate
 @REM     Hybrid Sleep
-@REM     Fast Startup
+@REM     Fast Startup (NOT WORK: Up time in Task Manager resets to zero (must be preserved between starts))
 @ECHO OFF
 REM POWERCFG /HIBERNATE OFF
 POWERCFG /HIBERNATE /SIZE 40
