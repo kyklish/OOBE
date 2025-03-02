4a42f16bf14d8968448275ab218a7ff63e384bb6
kyklish <kyklish@gmail.com>
2025-03-02 13:24:55 +0200

    PowerCfg

--- 130_AHK_Power_Scheme.ps1
+++ 130_AHK_Power_Scheme.ps1
@@ -1,7 +1,19 @@
-"Duplicate 'Balanced' Power Scheme..."
-"GUID: FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF"
-"Name: AHK"
+"Set TIMEOUT settings for 'Balanced' Power Scheme..."
 
-POWERCFG /DUPLICATESCHEME 381b4222-f694-41f0-9685-ff5bb260df2e FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF
-POWERCFG /CHANGENAME FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF "AHK" "Balanced power plan for AHK: CPU_Freq_Cores_Manager"
+$GUID_AHK = "FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF"
+$GUID_Balanced = "381b4222-f694-41f0-9685-ff5bb260df2e"
+$name = "AHK"
+
+POWERCFG /SETACTIVE $GUID_Balanced
+POWERCFG /CHANGE monitor-timeout-ac 45
+POWERCFG /CHANGE disk-timeout-ac 0
+POWERCFG /CHANGE standby-timeout-ac 0
+POWERCFG /CHANGE hibernate-timeout-ac 0
+
+"Duplicate 'Balanced' Power Scheme..."
+"New GUID: $GUID_AHK"
+"New Name: $name"
+
+POWERCFG /DUPLICATESCHEME $GUID_Balanced $GUID_AHK
+POWERCFG /CHANGENAME $GUID_AHK "$name" "Balanced power plan for AHK: CPU_Freq_Cores_Manager"
 Pause
--- 230_Windows_Firewall_Notifications.ps1
+++ 230_Windows_Firewall_Notifications.ps1
@@ -1,6 +1,6 @@
 # This command will disable notifications for all firewall profiles
 #   (Domain, Private, and Public)
 "Disable Windows Firewall Notifications..."
-"Make sure [Windows Firewall Control] app has disabled [Secure Profile] option!"
+"Make sure [Windows Firewall Control] app has disabled [Secure Profile] option (if it's installed already)!"
 Set-NetFirewallProfile -NotifyOnListen "False"
 Pause
