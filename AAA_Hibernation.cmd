4956a46db43310b72f3fc3162789d152f568676f
kyklish <kyklish@gmail.com>
2025-04-15 16:29:45 +0300

    some settings

--- AAA_Hibernation.cmd
+++ AAA_Hibernation.cmd
@@ -1,10 +1,10 @@
-@REM 40% is minimum: 16GB x 0.4 = 6.4GB
-@REM POWERCFG /AVAILABLESLEEPSTATES
-@REM The following sleep states are available on MY system:
-@REM     Standby (S3)
-@REM     Hibernate
-@REM     Hybrid Sleep
-@REM     Fast Startup (NOT WORK: Up time in Task Manager resets to zero (must be preserved between starts))
+:: 40% is minimum: 16GB x 0.4 = 6.4GB
+:: POWERCFG /AVAILABLESLEEPSTATES
+:: The following sleep states are available on MY system:
+::     Standby (S3)
+::     Hibernate
+::     Hybrid Sleep
+::     Fast Startup (NOT WORK: Up time in Task Manager resets to zero (must be preserved between starts))
 @ECHO OFF
 REM POWERCFG /HIBERNATE OFF
 POWERCFG /HIBERNATE /SIZE 40
