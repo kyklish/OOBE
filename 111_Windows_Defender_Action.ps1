5ca06aaaa5d5aab58e662e73866850bc944ab292
kyklish <kyklish@gmail.com>
2025-05-20 10:16:07 +0300

    WinDef

---
+++ 111_Windows_Defender_Action.ps1
@@ -0,0 +1,16 @@
+﻿# Disable Tamper Protection before this script???
+
+Set-MpPreference -LowThreatDefaultAction      Quarantine
+Set-MpPreference -ModerateThreatDefaultAction Quarantine
+Set-MpPreference -HighThreatDefaultAction     Quarantine
+Set-MpPreference -SevereThreatDefaultAction   Quarantine
+# Set-MpPreference -UnknownThreatDefaultAction  Quarantine
+Pause
+
+# Show Current Actions:
+# Get-MpPreference | Select-Object *action
+# Actions:
+# 0 - Default Action (Microsoft Desire)
+# 2 - Quarantine
+# 3 - Delete
+# 9 - Ignore
