02641282236ebcaac0c70d727dd7793ac5e6921b
kyklish <kyklish@gmail.com>
2026-02-24 15:15:53 +0200

    revert desktop resolution to NIRCMD

--- _MONITOR_APPLY_FullHD_.cmd
+++ _MONITOR_APPLY_FullHD_.cmd
@@ -2,5 +2,6 @@
 CD /D "%~dp0"
 :: .\DRIVERS\nircmd.exe setdisplay monitor:0 1920 1080 32 60 -allusers -updatereg
 :: .\DRIVERS\nircmd.exe setdisplay 1920 1080 32 60 -allusers -updatereg
+.\DRIVERS\nircmd.exe setdisplay 1920 1080 32 60 -allusers
 
-.\DRIVERS\GuMonSet32.exe 1920 1080 32 60
+:: .\DRIVERS\GuMonSet32.exe 1920 1080 32 60
