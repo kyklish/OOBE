d100bfbd379407fcfc9b6cbbc1a2ccd49491048f
kyklish <kyklish@gmail.com>
2026-04-21 14:13:07 +0300

    add .NET3

--- 600_Windows_Optional_Features.ps1
+++ 600_Windows_Optional_Features.ps1
@@ -1,4 +1,5 @@
 "Enable Windows Optional Features..."
 Enable-WindowsOptionalFeature -All -Online -FeatureName "DirectPlay"
+Enable-WindowsOptionalFeature -All -Online -FeatureName "NetFx3"
 Disable-WindowsOptionalFeature -Online -FeatureName "WindowsMediaPlayer"
 Pause
